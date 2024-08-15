adad7b2239d0bef28c8e8167c14efbb8fe67f98d
Web Master <webmaster@dc1rec.local>
2024-08-15 10:35:38 -0600

    debug carga v8

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -59,6 +59,7 @@
     @include('modales.modal_confirmacion')
     @include('modales.modal_no_mensajes')
     @include('modales.modal_carga')
+    @include('modales.modal_error_carga_datos')
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
