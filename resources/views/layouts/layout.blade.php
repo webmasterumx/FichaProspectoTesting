bfaa2279b04ba0031a088b5325828b3360a37ed7
Web Master <webmaster@dc1rec.local>
2024-11-09 13:33:51 -0600

    prueba de cache v4

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,3 +1,9 @@
+
+<?php
+  header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+  header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+?>
+
 <!doctype html>
 <html lang="en">
 
